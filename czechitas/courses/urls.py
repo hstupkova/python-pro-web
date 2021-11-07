43bf9c7ec4617eb407a8cde407320b31e829932e
Hana Stupková <hstupkova@gmail.com>
2021-11-07 11:23:53 +0100

    contacts and about views

--- czechitas/courses/urls.py
+++ czechitas/courses/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path("kontakty", views.ContactsView.as_view(), name="contacts"),
+    path("o-nas", views.AboutView.as_view(), name="about")
 ]

71565183004599f631b77d92d5da6a61385aab33
Hana Stupková <hstupkova@gmail.com>
2021-11-07 17:04:06 +0100

    security key deleted from settings

--- czechitas/czechitas/settings.py
+++ czechitas/czechitas/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hx0e@je&g=o6n_qa^$m+-qac(hc%qzf=b%la-8afb=cj^4$pks'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

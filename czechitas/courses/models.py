761ece425e7280f822981baab76447c5546f60c9
Hana Stupková <hstupkova@gmail.com>
2021-11-07 12:06:30 +0100

    first model: Course

--- czechitas/courses/models.py
+++ czechitas/courses/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+  name = models.CharField(max_length=100)
+  start = models.DateTimeField()
+  end = models.DateTimeField()
+  description = models.CharField(max_length=1000)
+  price = models.IntegerField()
